73e118ef742ff67c5d76a082cf163f5a42c24e2d
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-03-07 09:56:55 +0700

    Update and rename Ôn Thi to onthi.c

--- onthi.c
+++ onthi.c
@@ -16,4 +16,18 @@ int main(){
 }
 
 
+Bt13 Viết chương trình giải phương trình bậc nhất
+
+#include<stdio.h>
+
+int main(){
+  float x,a,b;
+  printf("Nhap a = ");scanf("%f", &a);
+  printf("Nhap b = ");scanf("%f", &b);
+  printf("Phuong trinh co dang %.1fx + %.1f = 0\nTim x\n",a,b);
+  x = -b/a;
+  printf("x = %.2f",x);
+  return 0;
+}
+
 
